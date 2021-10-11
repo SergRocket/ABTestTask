f3f8252aff5e23901632e5c43a052dd928e6d18e
SergQA <rockettester3107@gmail.com>
2021-10-11 19:16:23 +0300

    Adding conflict2

--- src/test/java/Tests/Algoritms.java
+++ src/test/java/Tests/Algoritms.java
@@ -193,16 +193,17 @@ isPalindrome("ABCDEG");
     }
     public static  boolean checktheSum(int givens){
         String s = Integer.toString(givens);
-        int[] count = new int[2];
+        int[] counter = new int[2];
         for (int i = 0; i < s.length(); i++) {
-            int digit = Integer.parseInt(s.substring(i, i + 1));
+            int digit = Integer.parseInt(
+                    s.substring(i, i + 1));
             int countIndex = (int)Math.floor(i * 2 / s.length());
-            count[countIndex] += digit;
+            counter[countIndex] += digit;
         }
         int half = s.length() / 2;
-        System.out.println("First Sum of " + s.substring(0, half) + ": " + count[0]);
-        System.out.println("Second Sum of " + s.substring(half) + ": " + count[1]);
-        if(count[0] == count[1]){
+        System.out.println("First Sum of " + s.substring(0, half) + ": " + counter[0]);
+        System.out.println("Second Sum of " + s.substring(half) + ": " + counter[1]);
+        if(counter[0] == counter[1]){
  return true;
         }
         return false;
